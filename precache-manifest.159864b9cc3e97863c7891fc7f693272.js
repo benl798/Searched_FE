539cdc1d3452f41471df63b4e1193a60fbb4a278
benl798 <ben.l7@hotmail.com>
2020-09-24 16:37:20 +1000

    Updates

---
+++ precache-manifest.159864b9cc3e97863c7891fc7f693272.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de419c83f694a44aa55d5a1c3263bdae",
+    "url": "/Searched_FE/index.html"
+  },
+  {
+    "revision": "f1460cdbc39713721d0c",
+    "url": "/Searched_FE/static/css/main.bf99e088.chunk.css"
+  },
+  {
+    "revision": "655a5b9f0d3e878f0d21",
+    "url": "/Searched_FE/static/js/2.8c113ac2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Searched_FE/static/js/2.8c113ac2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1460cdbc39713721d0c",
+    "url": "/Searched_FE/static/js/main.0a32586d.chunk.js"
+  },
+  {
+    "revision": "17bbaf6e6dfe7d814fb1",
+    "url": "/Searched_FE/static/js/runtime-main.d64fd3d1.js"
+  }
+]);
